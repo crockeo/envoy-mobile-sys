1b9b0e3e1ae3c7171ca8a1700ff6c5d3511bcf22
Cerek Hillen <chillen@lyft.com>
2021-01-11 15:47:47 -0500

    simplify build script / signpost instability

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # envoy-mobile-sys
 
-a [syscrate](https://kornel.ski/rust-sys-crate) for
-[envoy-mobile](https://github.com/envoyproxy/envoy-mobile).
+A **heavily unstable** syscrate for [envoy-mobile]((https://github.com/envoyproxy/envoy-mobile).
+I cannot emphasize enough that I do _not_ recommend you use this crate until it becomes more
+mature. Some reasons why:
+
+* The `envoy-mobile` submodule doesn't track upstream. It includes a much more modern version of
+  envoy that hasn't been extensively tested.
+* There is an additional patch in `envoy-mobile` to make it work with the fast-forwared version of
+  envoy.
+* I literally haven't managed to test it yet.
--- build.rs
+++ build.rs
@@ -5,6 +5,51 @@ use std::io;
 use std::path::PathBuf;
 use std::process;
 
+fn main() -> Result<()> {
+    println!("cargo:rerun-if-changed=envoy-mobile/library/common/main_interface.h");
+    println!("cargo:rerun-if-changed=envoy-mobile/library/common/types/c_types.h");
+    println!("cargo:rustc-link-lib=envoy_mobile.so");
+    println!("cargo:rustc-link-search=./bazel-bin");
+
+    let out_dir = PathBuf::from(env::var("OUT_DIR").expect("requires $OUT_DIR env var"));
+    build_envoy_mobile(&out_dir)?;
+    build_bindings(&out_dir)?;
+
+    Ok(())
+}
+
+fn build_envoy_mobile(out_dir: &PathBuf) -> Result<()> {
+    let mut builder = process::Command::new("bazel")
+        .stdout(process::Stdio::inherit())
+        .arg(format!(
+            "--output_user_root={}",
+            out_dir.join("bazel-out").to_str().unwrap()
+        ))
+        .arg("build")
+        .arg("//:envoy_mobile.so")
+        .spawn()?;
+
+    let exit_status = builder.wait()?;
+    if !exit_status.success() {
+        return Err(BuildError::BazelError(exit_status.code()));
+    }
+
+    Ok(())
+}
+
+fn build_bindings(out_dir: &PathBuf) -> Result<()> {
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .clang_arg("-I./envoy-mobile")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .map_err(|_| BuildError::BindingError)?;
+
+    bindings.write_to_file(out_dir.join("bindings.rs"))?;
+
+    Ok(())
+}
+
 #[derive(Debug)]
 enum BuildError {
     IOError(io::Error),
@@ -19,50 +64,3 @@ impl From<io::Error> for BuildError {
 }
 
 type Result<T> = core::result::Result<T, BuildError>;
-
-fn main() -> Result<()> {
-    let required_files = vec![
-        "envoy-mobile/library/common/main_interface.h",
-        "envoy-mobile/library/common/types/c_types.h",
-    ];
-    for required_file in required_files.into_iter() {
-        println!("cargo:rerun-if-changed={}", required_file);
-    }
-
-    println!("cargo:rustc-link-lib=envoy_mobile.so");
-    println!("cargo:rustc-link-search=./bazel-bin");
-
-    build_envoy_mobile()?;
-    build_bindings()?;
-
-    Ok(())
-}
-
-fn build_envoy_mobile() -> Result<()> {
-    let mut builder = process::Command::new("bazel")
-        .stdout(process::Stdio::inherit())
-        .arg("build")
-        .arg("//:envoy_mobile.so")
-        .spawn()?;
-
-    let exit_status = builder.wait()?;
-    if !exit_status.success() {
-        return Err(BuildError::BazelError(exit_status.code()));
-    }
-
-    Ok(())
-}
-
-fn build_bindings() -> Result<()> {
-    let bindings = bindgen::Builder::default()
-        .header("wrapper.h")
-        .clang_arg("-I./envoy-mobile")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate()
-        .map_err(|_| BuildError::BindingError)?;
-
-    let out_path = PathBuf::from(env::var("OUT_DIR").expect("requires $OUT_DIR env var"));
-    bindings.write_to_file(out_path.join("bindings.rs"))?;
-
-    Ok(())
-}
