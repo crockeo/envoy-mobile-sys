eab603719f840f2a251a7c40f484e4e20e611939
Cerek Hillen <chillen@lyft.com>
2021-05-16 14:17:41 -0700

    remove bazel build

--- build.rs
+++ build.rs
@@ -1,10 +1,8 @@
 extern crate bindgen;
 
 use std::env;
-use std::fs::remove_file;
 use std::io;
 use std::path::PathBuf;
-use std::process;
 
 fn main() -> Result<()> {
     let out_dir = PathBuf::from(env::var("OUT_DIR").expect("requires $OUT_DIR env var"));
@@ -12,38 +10,8 @@ fn main() -> Result<()> {
     println!("cargo:rerun-if-changed=envoy-mobile/library/common/main_interface.h");
     println!("cargo:rerun-if-changed=envoy-mobile/library/common/types/c_types.h");
     println!("cargo:rustc-link-lib=envoy_mobile");
-    println!(
-        "cargo:rustc-link-search={}/bazel-bin",
-        out_dir.to_str().unwrap()
-    );
 
-    build_envoy_mobile(&out_dir)?;
     build_bindings(&out_dir)?;
-
-    Ok(())
-}
-
-fn build_envoy_mobile(out_dir: &PathBuf) -> Result<()> {
-    let mut builder = process::Command::new("bazel")
-        .arg("build")
-        .arg(format!(
-            "--symlink_prefix={}/bazel-",
-            out_dir.to_str().unwrap()
-        ))
-        .arg("//:libenvoy_mobile.so")
-        .spawn()?;
-
-    // bazel still creates a bazel-out no matter what you tell it to do. so we just remove it after
-    // the fact :)
-    //
-    // note that we don't care if it succeeds in removing bazel-out, because Cargo will just fail
-    // if it exists
-    let _ = remove_file("bazel-out");
-
-    let exit_status = builder.wait()?;
-    if !exit_status.success() {
-        return Err(BuildError::BazelError(exit_status.code()));
-    }
 
     Ok(())
 }
@@ -64,7 +32,6 @@ fn build_bindings(out_dir: &PathBuf) -> Result<()> {
 #[derive(Debug)]
 enum BuildError {
     IOError(io::Error),
-    BazelError(Option<i32>),
     BindingError,
 }
 
